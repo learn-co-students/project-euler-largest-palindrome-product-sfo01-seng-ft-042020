795b427daff7e084ebb870b37af712d913c8d22f
Don Mallory <dmm333@gmail.com>
2020-08-22 13:27:49 -0500

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,18 @@
-# Implement your procedural solution here!
+def largest_palindrome_product
+    max = 0
+    for i in 900..1000
+        for j in 900..1000
+            product = i*j
+            if is_palindrome?(product) && product > max
+                puts "New Largest Found: #{product}"
+                max = product
+            end
+        end
+    end
+    max
+end
+
+def is_palindrome?(num)
+    string = num.to_s
+    string == string.reverse
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,22 @@
-# Implement your object-oriented solution here!
+class LargestPalindromeProduct
+    attr_reader :answer
+    
+    def initialize
+        max = 0
+        for i in 900..1000
+            for j in 900..1000
+                product = i*j
+                if is_palindrome?(product) && product > max
+                    puts "New Largest Found: #{product}"
+                    max = product
+                end
+            end
+        end
+        @answer = max
+    end
+    
+    def is_palindrome?(num)
+        string = num.to_s
+        string == string.reverse
+    end
+end
